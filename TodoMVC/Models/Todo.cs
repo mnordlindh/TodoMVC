55bfba383576d7b69fa4ff3bcff66d99606bae6f
EuroDollar <mattiasnordlindh@gmail.com>
2014-08-28 14:22:18 +0200

    Adds DbContext and Todo-model.

---
+++ TodoMVC/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoMVC.Models {
+    public class Todo {
+        public string Text { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime TimeCreated { get; set; }
+    }
+}
---
+++ TodoMVC/Models/TodoContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoMVC.Models {
+    /// <summary>
+    /// Derived context.
+    /// </summary>
+    public class TodoContext : DbContext {
+        public TodoContext()
+            : base("DefaultConnection") {
+            // Database strategy
+                Database.SetInitializer<TodoContext>(new SocialMediaDbInit());
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        // If you want to try or need to (some use cases) use fluent API this is the place!
+        // Reference: http://blogs.msdn.com/b/adonet/archive/2010/12/14/ef-feature-ctp5-fluent-api-samples.aspx
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    /// <summary>
+    /// Derived database strategy
+    /// </summary>
+    class SocialMediaDbInit : DropCreateDatabaseIfModelChanges<TodoContext> {
+        protected override void Seed(TodoContext context) {
+            base.Seed(context);
+
+            // Seed your data here!
+        }
+    }
+}
