c062ce463171c03cb29fc8d98b2a6e235eb8aedc
EuroDollar <mattiasnordlindh@gmail.com>
2014-08-29 15:19:14 +0200

    Adds comments to the todo-repository.

--- TodoMVC/Repositories/TodoRepository.cs
+++ TodoMVC/Repositories/TodoRepository.cs
@@ -12,25 +12,44 @@ namespace TodoMVC.Repositories {
             _context = ctx;
         }
 
+        /// <summary>
+        /// Add a new todo to the repository.
+        /// </summary>
+        /// <param name="item">The todo to add.</param>
         public void Add(Todo item) {
             _context.Todos.Add(item);
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Remove a todo from the repository.
+        /// </summary>
+        /// <param name="item">The todo to remove.</param>
         public void Remove(Todo item) {
             _context.Entry(item).State = System.Data.Entity.EntityState.Deleted;
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Finds a todo based on its id, if no todo is found null is returned.
+        /// </summary>
+        /// <param name="id">The id of the todo.</param>
         public Todo Find(int id) {
             return _context.Todos.Find(id);
         }
 
+        /// <summary>
+        /// Edit a todo in the repository.
+        /// </summary>
+        /// <param name="item">The edited todo.</param>
         public void Edit(Todo item) {
             _context.Entry(item).State = System.Data.Entity.EntityState.Modified;
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Get all todos from the repository.
+        /// </summary>
         public IEnumerable<Todo> All() {
             return _context.Todos.ToList();
         }
